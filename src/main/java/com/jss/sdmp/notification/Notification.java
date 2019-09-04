bba05a1768862090a2fd1a319bdc74c36c5de1cd
Atishaya Jain <atishaya.11@live.com>
2019-09-04 16:38:30 +0530

    Notification feature prototype test

--- src/main/java/com/jss/sdmp/notification/Notification.java
+++ src/main/java/com/jss/sdmp/notification/Notification.java
@@ -1,8 +1,27 @@
 package com.jss.sdmp.notification;
 
+import com.jss.sdmp.notification.model.NotificationType;
+import com.jss.sdmp.users.model.User;
+
 public class Notification {
+
+    private User user;
 
     private NotificationType notificationType;
 
+    public User getUser() {
+        return user;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public NotificationType getNotificationType() {
+        return notificationType;
+    }
+
+    public void setNotificationType(NotificationType notificationType) {
+        this.notificationType = notificationType;
+    }
 }
