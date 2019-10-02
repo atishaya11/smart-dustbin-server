27c751f5d03eb2d14ff770fd3dcbcd6be5610e50
atishaya11 <atishaya.11@live.com>
2019-10-02 14:47:40 +0530

    Bin data test

--- src/main/java/com/jss/sdmp/data/BinDataReceiver.java
+++ src/main/java/com/jss/sdmp/data/BinDataReceiver.java
@@ -1,6 +1,11 @@
 package com.jss.sdmp.data;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jss.sdmp.data.dto.BinDataDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class BinDataReceiver {
 
-    private BinDataHandler binDataHandler;
+    private final BinDataHandler binDataHandler;
+
+    private static final Logger logger = LoggerFactory.getLogger(BinDataReceiver.class);
+
+    @Autowired
+    public BinDataReceiver(BinDataHandler binDataHandler) {
+        this.binDataHandler = binDataHandler;
+    }
 
     @GetMapping("/data")
     public void receive(BinDataDto binData) {
-
+        try {
+            logger.info(new ObjectMapper().writeValueAsString(binData));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/com/jss/sdmp/data/dto/BinDataDto.java
+++ src/main/java/com/jss/sdmp/data/dto/BinDataDto.java
@@ -3,8 +3,42 @@ package com.jss.sdmp.data.dto;
 public class BinDataDto {
 
     private String binId;
+
     private double latitude;
+
     private double longitude;
 
+    private double percentage;
 
+    public String getBinId() {
+        return binId;
+    }
+
+    public void setBinId(String binId) {
+        this.binId = binId;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
 }
